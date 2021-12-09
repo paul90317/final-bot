00d17f0d3f1aae0893fa781349d3a60343ec6894
paul90317 <paul2309825@gmail.com>
2021-12-10 02:28:34 +0800

    123

--- app.py
+++ app.py
@@ -18,13 +18,12 @@ app = Flask(__name__, static_url_path="")
 # get channel_secret and channel_access_token from your environment variable
 channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
 channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
 if channel_secret is None:
     print("Specify LINE_CHANNEL_SECRET as environment variable.")
-    sys.exit(1)
 if channel_access_token is None:
     print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
-    sys.exit(1)
-
+    
 line_bot_api = LineBotApi(channel_access_token)
 parser = WebhookParser(channel_secret)
 
