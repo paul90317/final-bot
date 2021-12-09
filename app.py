0a4e1c40a3a1df1970ab7f53c6a8ecf1f09b55b4
paul90317 <paul2309825@gmail.com>
2021-12-10 02:51:18 +0800

    123

--- app.py
+++ app.py
@@ -1,11 +1,91 @@
-from flask import Flask,render_template
 
-app=Flask(__name__)
+import os
+import sys
+from flask import Flask, jsonify, request, abort, send_file
 
+"""
+from dotenv import load_dotenv
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
-@app.route('/')
+from utils import send_text_message
+
+load_dotenv()
+"""
+
+app = Flask(__name__)
+
+"""
+# get channel_secret and channel_access_token from your environment variable
+channel_secret = os.getenv("LINE_CHANNEL_SECRET", None)
+channel_access_token = os.getenv("LINE_CHANNEL_ACCESS_TOKEN", None)
+
+if channel_secret is None:
+    print("Specify LINE_CHANNEL_SECRET as environment variable.")
+if channel_access_token is None:
+    print("Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.")
+
+line_bot_api = LineBotApi(channel_access_token)
+parser = WebhookParser(channel_secret)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+
+        line_bot_api.reply_message(
+            event.reply_token, TextSendMessage(text=event.message.text)
+        )
+
+    return "OK"
+
+@app.route("/webhook", methods=["POST"])
+def webhook_handler():
+    signature = request.headers["X-Line-Signature"]
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info(f"Request body: {body}")
+
+    # parse webhook body
+    try:
+        events = parser.parse(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    # if event is MessageEvent and message is TextMessage, then echo text
+    for event in events:
+        if not isinstance(event, MessageEvent):
+            continue
+        if not isinstance(event.message, TextMessage):
+            continue
+        if not isinstance(event.message.text, str):
+            continue
+        print(f"REQUEST BODY: \n{body}")
+        send_text_message(event.reply_token, f"{event.message.text},too")
+
+    return "OK"
+"""
+
+@app.route("/")
 def home():
-    return 'hello world'
+    return send_file("fsm.png", mimetype="image/png")
 
-if __name__=="__main__": #直接執行
+
+if __name__ == "__main__":
     app.run()
